62c9d89b0daff4636f875d342a427d5f59fc8082
kellermack <45441341+kellermack@users.noreply.github.com>
2021-01-31 21:56:24 -0500

    edit button

--- comments.php
+++ comments.php
@@ -20,6 +20,26 @@ function getComments($conn) {
             echo $row['uid'];
             echo $row['date'];
             echo nl2br($row['message']);
-        echo "</p></div>";
+        echo "</p>
+            <form class = 'edit-form' method= 'POST' action = 'editcomment.php'>
+                <input type='hidden' name='cid' value='".$row['cid']."'>
+                <input type='hidden' name='uid' value='".$row['uid']."'>
+                <input type='hidden' name='date' value='".$row['date']."'>
+                <input type='hidden' name='message' value='".$row['message']."'>
+                <button>Edit</button>
+            </form>
+        </div>";
+    }
+}
+
+function editComments($conn) {
+    if (isset($_POST['commentSubmit'])) {
+       $uid = $_POST['uid'];
+       $date = $_POST['date'];
+       $message = $_POST['message'];
+
+       $sql = "INSERT INTO comments (uid, date, message)  
+       VALUES ('$uid', '$date', '$message')";
+       $result = $conn->query($sql);
     }
 }
