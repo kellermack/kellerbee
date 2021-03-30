648232d77888dbbbcb8cd9a52793775ed2d65260
kellermack <45441341+kellermack@users.noreply.github.com>
2021-03-29 23:11:23 -0400

    shopping cart

--- Storepage.php
+++ Storepage.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Store Page</title>
     <link rel="stylesheet" href="Storepage.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
     <h1>Honey Pot</h1>
@@ -13,6 +14,8 @@
     <nav class="product-filter">
 
 <h1>Honey</h1>
+<i class="fa fa-shopping-cart"></i>
+<button style="font-size:24px">Button <i class="fa fa-shopping-cart"></i></button>
 
 <div class="sort">
 
@@ -41,6 +44,10 @@
   <div class="product-card">
     <div class="product-image">
       <img src="Images/honey.jpeg">
+      <img src="Images/honey.jpeg">
+      <img src="Images/honey.jpeg">
+      <img src="Images/honey.jpeg">
+      
     </div>
     <div class="product-info">
       <h5>1 Pint Honey</h5>
