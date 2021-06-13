70268589e3414c96907b7797646b46c5c69f84a9
kellermack <45441341+kellermack@users.noreply.github.com>
2021-06-13 18:48:37 -0400

    Add files via upload

--- signup.php
+++ signup.php
@@ -1,61 +1,18 @@
 <?php
 
-    session_start();
-    include('loginConfig.php');
-    if (isset($_POST['register'])) {
-        $username = $_POST['username'];
-        $email = $_POST['email'];
-        $password = $_POST['password'];
-        $password_hash = password_hash($password, PASSWORD_BCRYPT);
-        $query = $conn->prepare("SELECT * FROM users WHERE email=:email");
-       
-        $query->bindParam("email", $email, PDO::PARAM_STR);
-        $query->execute();
-        if ($query->rowCount() > 0) {
-            echo '<p class="error">The email address is already registered!</p>';
-        }
-        if ($query->rowCount() == 0) {
-            $query = $conn->prepare("INSERT INTO users(username,password,email) VALUES (:username,:password_hash,:email)");
-            $query->bindParam("username", $username, PDO::PARAM_STR);
-            $query->bindParam("password_hash", $password_hash, PDO::PARAM_STR);
-            $query->bindParam("email", $email, PDO::PARAM_STR);
-            $result = $query->execute();
-            if ($result) {
-                echo '<p class="success">Your registration was successful!</p>';
-            } else {
-                echo '<p class="error">Something went wrong!</p>';
-            }
-        }
-        
-    }
-?>
+>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sign Up!</title>
-    <link rel="stylesheet" href="signup.css">
-</head>
-<body>
-<a href="index.php"><img src="Images/honeycomb.png" alt="Bee hive" width="5%" height="5%"></a>
- <h3>Back to the hive!</h3>
-<form method="post" action="" name="signup-form">
-<div class="form-element">
-<label>Username</label>
-<input type="text" name="username" pattern="[a-zA-Z0-9]+" required />
-</div>
-<div class="form-element">
-<label>Email</label>
-<input type="email" name="email" required />
-</div>
-<div class="form-element">
-<label>Password</label>
-<input type="password" name="password" required />
-</div>
-<button type="submit" name="register" value="register">Register</button>
-</form> 
-</body>
-</html>
+
+<section class = "signupform-form">
+    <h2>Login</h2>
+    <div class = "signup-form-form"
+    <form action = "includes/signup.inc.php" method = "post"> </form>
+        <input type = "text" name = "name" placeholder = "full name">
+        <input type = "text" name = "Email" placeholder = "Email">
+        <input type = "text" name = "uid" placeholder = "Username">
+        <input type = "password" name = "pwd" placeholder = "Password">
+        <input type = "password" name = "pwdrepeat" placeholder = "Repeat Password">
+        <button type = "submit" name = "submit">Sign Up</button>
+    </div>
+    </form>
+</section>
